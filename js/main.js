aefb330c4c9d5b055013b42d474b15854e8c4c05
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-03-12 21:08:09 +0300

    Bla-bla-bla

--- js/main.js
+++ js/main.js
@@ -5,10 +5,13 @@ window.onload = () => {
             crds[i].style.boxShadow = '0 14px 28px rgb(246, 5, 5), 0 10px 10px rgb(246, 5, 5)';
             crds[i].style.cursor = 'pointer';
             crds[i].style.height = '250px';
+            crds[i].querySelector('.card-img-top').style.filter = 'blur(3px) brightness(25%)';
+
         };
         crds[i].onmouseout = () => {
             crds[i].style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
             crds[i].style.height = '177px';
+            crds[i].querySelector('.card-img-top').style.filter = 'brightness(25%)';
         }
     }
 }
