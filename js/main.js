4d110ec8e757252f2b61cbaa9fd2f414a7b981fd
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-03-12 19:36:47 +0300

    Many thimgs have been done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    let crds = [document.getElementById('card_1'), document.getElementById('card_2'), document.getElementById('card_3'), document.getElementById('card_4'), document.getElementById('card_5')];
+    for (let i = 0; i < crds.length; i++) {
+        crds[i].onmouseover = () => {
+            crds[i].style.boxShadow = '0 14px 28px rgb(246, 5, 5), 0 10px 10px rgb(246, 5, 5)';
+            crds[i].style.cursor = 'pointer';
+            crds[i].style.height = '250px';
+        };
+        crds[i].onmouseout = () => {
+            crds[i].style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+            crds[i].style.height = '177px';
+        }
+    }
+}
---
+++ js/shows.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let date = new this.Date;
+    let d = document.getElementById('date');
+
+    d.innerHTML("Здесь будет дата");
+}
