e74be14f2a77c1ce16cf4ac0b8d28017e21598f8
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-04-01 21:48:31 +0300

    Schedule animation done

--- js/shows.js
+++ js/shows.js
@@ -1,1 +1,14 @@
-let row = document.getElementsByTagName('tr');
+let rows = [];
+$('tr').each(function() {
+    rows.push(this);
+})
+
+$(window).ready(function() {
+    for(let i=0; i<rows.length; i++) {
+        if ((i%2)===0) {
+    $(rows[i]).removeClass('hidden').addClass('animated slideInLeft slower')
+        } else {
+            $(rows[i]).removeClass('hidden').addClass('animated slideInRight slower')
+        }
+    }
+})
